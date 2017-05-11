102da7056a279afcd011d05f60f740252b6f3bda
kirby <kirby@grothuesfamily.com>
2017-05-10 20:57:25 -0500

    More Documentation
    Added To Do list
    Added more documentation for Format

--- src/teamAPI/Format.java
+++ src/teamAPI/Format.java
@@ -3,27 +3,58 @@ package teamAPI;
 import java.util.*;
 import teamAPI.teamExceptionAPI.*;
 
-public class Format {
+public class Format {	//Dual Purpose Class
+	/*
+	 * Formats for the competition's layout are stored so all classes can access them
+	 */
 	static Format generalFormat;
 	static Format pointsFormat;
 	static Format penaltiesFormat;
 	
+	/*
+	 * Stores the user provided layout for this format object
+	 */
 	private ArrayList<Statistic> format;
 	
 	
+	
+	/*
+	 * Constructor for creating a format
+	 * Requirements: statistics (ArrayList<Statistic>)
+	 */
 	public Format(ArrayList<Statistic> format) throws InvalidFormatException{
 		if (format.size()>0) this.format = format;
 		else throw new InvalidFormatException();
 	}
 	
+	
+	
+	/*
+	 * Method for getting the size of the format
+	 * Returns: size (int)
+	 */
 	public int getSize() {
 		return format.size();
 	}
 	
+	
+	
+	/*
+	 * Method for getting the format's statistics
+	 * Returns: statistics (ArrayList<Statistic>)
+	 */
 	public ArrayList<Statistic> getStatistics() {
 		return format;
 	}
 	
+	
+	
+	/*
+	 * Method for getting the position of a user specified statistic
+	 * Requirements: format (Format)
+	 * 				 statistic name (String)
+	 * Returns: position (int)
+	 */
 	public static int getStatisticPosition(Format format, String dataName) throws CouldNotFindException {
 		for(int x=0;x<format.getStatistics().size();x++) {
 			if(format.getStatistics().get(x).getName().equals(dataName)) return x;
@@ -31,6 +62,13 @@ public class Format {
 		throw new CouldNotFindException();
 	}
 	
+	
+	
+	/*
+	 * Method for checking if a statistic has a valid name based on the specified format
+	 * Requirements: statistic (Statistic)
+	 * Returns: is good (boolean) 
+	 */
 	public boolean isValidStatistic(Statistic stat) {
 		boolean isGood = false;
 		for (Statistic s:format) {
@@ -41,6 +79,13 @@ public class Format {
 		return isGood;
 	}
 	
+	
+	
+	/*
+	 * Method for checking if a statistic has a valid name in any of the static formats
+	 * Requirements: statistic (Statistic)
+	 * Returns: is good (boolean) 
+	 */
 	public static boolean isAnyValidStatistic(Statistic stat) {
 		boolean isGood = false;
 		for (Statistic s:generalFormat.getStatistics()) {
@@ -61,6 +106,14 @@ public class Format {
 		return isGood;
 	}
 	
+	
+	
+	/*
+	 * Method for setting the static formats
+	 * Requirements: general format (Format)
+	 * 				 points format (Format)
+	 * 				 penalties format (Format)
+	 */
 	public static void setFormat(Format generalFormat, Format pointsFormat, Format penaltiesFormat) {
 		Format.generalFormat = generalFormat;
 		Format.pointsFormat = pointsFormat;
