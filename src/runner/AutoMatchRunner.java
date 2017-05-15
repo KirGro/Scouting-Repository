122a1efc6080cc9235bf5a240dae24864b26f8eb
KirGro <kirby@grothuesfamily.com>
2017-05-15 10:59:37 -0500

    Made autoMatchRunner work
    Added a constructor
    Added autoAddMatch

--- src/runner/AutoMatchRunner.java
+++ src/runner/AutoMatchRunner.java
@@ -1,5 +1,28 @@
 package runner;
 
 public class AutoMatchRunner {
-
+  private TeamDataBase tdb; //Stores the reference to the database so it can add data sets to it
+  
+  
+  
+  /*
+   * Constructor for AutoMatchRunner
+   * Requirements: database (TeamDataBase)
+   */
+  public AutoMatchRunner(TeamDataBase tdb) {
+    this.tdb = tdb;
+  }
+  
+  
+  
+  /*
+   * Method for automagically adding a match to the database, regardless
+   * Requirements: match (MatchResult)
+   */
+  public void autoAddMatch(MatchResult mr) {
+    try {
+      tdb.getTeamReference(mr.getData(Format.generalInfo, "Team Number").getData()).addMatch(mr);
+    } catch (CouldNotFindException cnfe) {
+      tdb.addTeam(mr.getData(Format.generalInfo, "Team Number").getData(),new ArrayList<MatchResult>(){{add(mr);}});
+  }
 }
